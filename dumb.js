c668e584c9820e76e3de3f6684cd374a89b7c712
AmyChen159 <144455940+AmyChen159@users.noreply.github.com>
2023-10-25 09:24:45 -0400

    m

--- dumb.js
+++ dumb.js
@@ -3,24 +3,24 @@ const DOMselectors = {
     firstName: document.querySelector(".first-name"),
     h2s: document.querySelectorAll("h2"),
 };
-function makeAlbum(){
-    //return an object
-}
-function addCard(Album){}
-DOMselectors.form.addEventListener("submit", function(event){
-    event.preventDefault();
-    document.querySelector(".gallery")
-    .insertAdjacentElement(
-        "afterbegin",
-        `<div class="card"><div class="card-title"> $(Album.name)</div></div>`
-    );
-    console.log(DOMselectors.firstName.value);
-    DOMselectors.h2s.forEach((el)=> el.textContent = DOMselectors.firstName.value
+// function makeAlbum(){
+//     //return an object
+// }
+// function addCard(Album){}
+// DOMselectors.form.addEventListener("submit", function(event){
+//     event.preventDefault();
+//     document.querySelector(".gallery")
+//     .insertAdjacentElement(
+//         "afterbegin",
+//         `<div class="card"><div class="card-title"> $(Album.name)</div></div>`
+//     );
+//     console.log(DOMselectors.firstName.value);
+//     DOMselectors.h2s.forEach((el)=> el.textContent = DOMselectors.firstName.value
     
-    //const Album = makeAlbum();
-    //addcard()
-    //clearFields()
-    //addRemoveButtons
-    );
-});
-//dont use onclick(?)
+//     //const Album = makeAlbum();
+//     //addcard()
+//     //clearFields()
+//     //addRemoveButtons
+//     );
+// });
+// //dont use onclick(?)
