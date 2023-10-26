f6b88e4bc5dde02e94db73fb0b7a687598ca30f9
AmyChen159 <144455940+AmyChen159@users.noreply.github.com>
2023-10-25 22:49:29 -0400

    huh

--- dumb.js
+++ dumb.js
@@ -1,8 +1,8 @@
-const DOMselectors = {
+/*const DOMselectors = {
     form: document.querySelector("#form"),
     firstName: document.querySelector(".first-name"),
     h2s: document.querySelectorAll("h2"),
-};
+}; */
 // function makeAlbum(){
 //     //return an object
 // }
@@ -24,3 +24,36 @@ const DOMselectors = {
 //     );
 // });
 // //dont use onclick(?)
+
+const DOMselectors = {
+    form: document.querySelector("#form"),
+    name: document.querySelector(".name"),
+    element: document.querySelector(".element"),
+    image: document.querySelector(".image"),
+    gallery: document.querySelector(".gallery"),
+    submit: document.querySelector("#submit"),
+}
+
+DOMselectors.submit.addEventListener("click", function(event){
+    event.preventDefault()
+    const name = DOMselectors.name.value
+    const element = DOMselectors.element.value
+    const image = DOMselectors.image.value
+
+    if (name && element && image) {
+        createCard()
+    }
+})
+
+function createCard() {
+    const holder = document.createElement("div")
+    holder.classList.add("holder")
+   
+    const name = `${name}`
+    const element = `${element}`
+    const image = document.createElement("image")
+    image.scr = image
+
+    holder.insertAdjacentHTML("afterbegin", name)
+    holder.insertAdjacentHTML("afterbegin", element)
+}
